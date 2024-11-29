98a9d10a9e5d890b4d549066d399085ac03a7607
EthanBaalham <146436503+EthanBaalham@users.noreply.github.com>
2024-11-29 13:37:49 +0000

    Basic Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Canvas;
+    bool Paused = false;   
+
+    public void Start() {
+        Canvas.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (Paused == true) {
+                Canvas.SetActive(false);
+                Time.timeScale = 1.0f;
+                Paused = false;
+            }
+            else {
+                Canvas.SetActive(true);
+                Time.timeScale = 0.0f;
+                Paused = true;
+            }
+        }
+    }
+}
