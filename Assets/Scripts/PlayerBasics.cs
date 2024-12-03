39e3d595a302acfcf82a75c644b68fb521251ceb
RowanCrawley <s4308300@glos.ac.uk>
2024-12-03 03:29:21 +0000

    dropboxes are better and player can stick to walls
    also changed the test level a bit

--- Assets/Scripts/PlayerBasics.cs
+++ Assets/Scripts/PlayerBasics.cs
@@ -4,19 +4,21 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 //Created by Rowan
-//Player movement and basic interactions
+//Player movement and basic interactions with platforms
 public class PlayerBasics : MonoBehaviour {
     private float charge;
     public float temp, jumpPower, chargeMax, strafeAmount;
     public Vector2 gravity;
-    bool rightJump, leftJump, jumping = false, DBCollision;
+    bool interacting, dropping, rightJump, leftJump, jumping = false;
     Rigidbody2D body;
-    public int currZoom = 10;
+    Vector2 startGravity;
+    public int currZoom = 10,chargeSpd = 5;
     GameObject platform;
     public Slider chargeBar;
     private void Start() {
         Physics2D.gravity = gravity;
         body = GetComponent<Rigidbody2D>();
+        startGravity = gravity;
     }
     private void FixedUpdate() {
         if (jumping) {
@@ -25,8 +27,8 @@ public class PlayerBasics : MonoBehaviour {
                 chargeBar.value = chargeMax;
             }
             else {
-                chargeBar.value += Time.deltaTime * 5;
-                charge += Time.deltaTime * 5;
+                chargeBar.value += Time.deltaTime * chargeSpd;
+                charge += Time.deltaTime * chargeSpd;
             }
         }
         if(leftJump) {
@@ -38,6 +40,14 @@ public class PlayerBasics : MonoBehaviour {
             if (body.velocity.y > 0) {
                 body.AddForce(new Vector2(strafeAmount, 0));
             }
+        }
+    }
+    public void Interact(InputAction.CallbackContext callback) {
+        if (callback.started) {
+            interacting = true;
+        }
+        if (callback.canceled) {
+            interacting = false;
         }
     }
     public void LeftMove(InputAction.CallbackContext callback) {
@@ -61,9 +71,10 @@ public class PlayerBasics : MonoBehaviour {
             jumping = true;
         }
         if (callback.canceled) {
+
             temp = jumpPower;
             jumpPower *= charge;
-            if (GetComponent<Rigidbody2D>().velocity == Vector2.zero) {
+            if (GetComponent<Rigidbody2D>().velocity.magnitude <= 2) {
                 if (rightJump == true) {
                     body.velocity = new Vector2(jumpPower / 2, jumpPower);
                 }
@@ -80,25 +91,53 @@ public class PlayerBasics : MonoBehaviour {
             chargeBar.value = 1;
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("dropBox")){
-            DBCollision = true;
-            platform = collision.gameObject;
-        }
-    }private void OnCollisionExit2D(Collision2D collision) {
-        if (collision.transform.CompareTag("dropBox")){
-            DBCollision = false;
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("dropBox")) {
+            if(body.velocity.y > 0 || dropping) {
+                Debug.Log("disabled box");
+                collision.gameObject.GetComponent<Collider2D>().enabled = false;
+                StartCoroutine(WaitEnable(collision.gameObject, 1.0f));
+            }
         }
     }
-    public void Drop(InputAction.CallbackContext callback) {  
-        if (callback.started) {
-            if (DBCollision) {
-                platform.gameObject.GetComponent<Collider2D>().enabled = false;
-                StartCoroutine(Wait(1.0f));
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("wall")) {
+            if (interacting) {
+                body.velocity = new Vector2(0, 0);
+                Physics2D.gravity = new Vector2(0, 0);
             }
+            
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("wall")) {
+            if (!interacting) {
+                Physics2D.gravity = startGravity;
+            }
+        }
+        if (collision.gameObject.CompareTag("dropBox")) {
+            if(dropping) {
+                collision.gameObject.GetComponent<Collider2D>().enabled = false;
+                StartCoroutine(WaitEnable(collision.gameObject, 1.0f));
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("wall")) {
+            Physics2D.gravity = startGravity;
+            interacting = false;
+        }
+    }
+    public void Drop(InputAction.CallbackContext callback) {
+        if (callback.started) {
+            dropping = true;
+        } 
+        if (callback.canceled) {
+            dropping = false;
         } 
     }
-    public IEnumerator Wait(float t) {
+    public IEnumerator WaitEnable(GameObject platform, float t) {
+        Debug.Log("waiting");
         yield return new WaitForSeconds(t);
         platform.gameObject.GetComponent<Collider2D>().enabled = true;
         platform = null;
