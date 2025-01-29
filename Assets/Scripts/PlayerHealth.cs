3dd06071004850cc308fc0ff9c6cdf943047aae1
EthanBaalham <146436503+EthanBaalham@users.noreply.github.com>
2025-01-29 17:27:48 +0000

    Health is now taken away from a circle.
    i need to do this with multiple circles but for now it is just set up in one circle.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,22 +5,28 @@ using UnityEngine.SceneManagement;
 //Created by Ethan
 public class PlayerHealth : MonoBehaviour
 {
-    public Slider healthSlider;
+    public Image healthHeart;
     public float currentHealth = 100f;
     public float maxHealth = 100f;
-    // setting my variables so it will know what the players max health can be and what the current health is. 
+    private bool isDamaged = false;   
+    // Setting up the correct Variables and using an image to display the players Health. 
     void Start() {
-        healthSlider.maxValue = maxHealth;
-        healthSlider.value = currentHealth;
-        // making sure when the game starts that the player  and the slider are matched up correctly. 
+        healthHeart.fillAmount = currentHealth / maxHealth;
+        // making sure when the game starts that the player is linked up to the image so it knows how to take damage and from what object.
     }
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Enemies")) {
-            TakeDamage(15f); 
-        }
-        healthSlider.value = currentHealth;
+            DamageTaken(25f); 
+            isDamaged = true;
+        }// anything with the tag enemies on it will take 25 health away from the player. 
     }
-    public void TakeDamage(float damage) {
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Enemies")) {
+            isDamaged = false;  //I have added in a boolean that will determine if the player is in the collider if it is true then damage the player if not then dont.
+                                  //I added this as it seems if they was still touching then the player would just die as it was always in the collider.
+        }
+    }
+    public void DamageTaken(float damage) {
         currentHealth -= damage;
         if (currentHealth < 0) {
             currentHealth = 0; 
@@ -28,6 +34,8 @@ public class PlayerHealth : MonoBehaviour
         if (currentHealth == 0) {
             SceneManager.LoadSceneAsync(3);
         }
-        // damage control function so this will determine how much health to take from the player. 
+        healthHeart.fillAmount = currentHealth / maxHealth;
+        // damage control function so this will determine how much health to take from the player and when the players health reaches 0 to enter respawn scene. 
     }
+    // i need to do this with multiple circles not just 1 :(.
 }
