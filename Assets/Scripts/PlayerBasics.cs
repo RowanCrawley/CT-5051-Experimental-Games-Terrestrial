c8b3d3b00eeb6bb01d0755621b5b446c607d6039
RowanCrawley <s4308300@glos.ac.uk>
2025-02-06 19:20:03 +0000

    unity should be set up for wiimotes now
    if someone with a wiimote could test this that would be great
    This is a link to the video im following if you need help connecting the controller. For now, it should just light up the first LED
    https://www.youtube.com/watch?v=LHbRlL8SBgE

--- Assets/Scripts/PlayerBasics.cs
+++ Assets/Scripts/PlayerBasics.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using WiimoteApi;
 
 //Created by Rowan
 //Player movement and basic interactions with platforms
 public class PlayerBasics : MonoBehaviour
 {
+    //generic vars for jumping
     private float temp;
     public float charge, jumpPower, chargeMax, strafeAmount;
     public Vector2 gravity;
@@ -15,85 +17,66 @@ public class PlayerBasics : MonoBehaviour
     Rigidbody2D body;
     Vector2 startGravity;
     public int currZoom = 10, chargeSpd = 5;
-    GameObject platform;
     public Slider chargeBar;
+    Wiimote mote;
     private void Start()
     {
+        chargeBar = GameObject.Find("ChargeBar").GetComponent<Slider>();
         Physics2D.gravity = gravity;
         body = GetComponent<Rigidbody2D>();
         startGravity = gravity;
+        WiimoteManager.FindWiimotes();
+        if (WiimoteManager.Wiimotes != null) {
+            mote = WiimoteManager.Wiimotes[0];
+            mote.SendPlayerLED(true,false,false,false);//lights up first light on controller
+        }
+        
     }
-    private void Update()
-    {
-        if (jumping)
-        {
-            if (charge > chargeMax)
-            {
+    private void Update(){
+        if (jumping){
+            if (charge > chargeMax){
                 charge = chargeMax;
                 chargeBar.value = chargeMax;
             }
-            else
-            {
+            else{
                 chargeBar.value += Time.deltaTime * chargeSpd;
                 charge += Time.deltaTime * chargeSpd;
             }
         }
-        if (leftJump)
-        {
-            if (body.velocity.y > 0)
-            {
+        if (leftJump){
+            if (body.velocity.y > 0){
                 body.AddForce(new Vector2(-strafeAmount, 0));
             }
         }
-        if (rightJump)
-        {
-            if (body.velocity.y > 0)
-            {
+
+        if (rightJump){
+            if (body.velocity.y > 0){
                 body.AddForce(new Vector2(strafeAmount, 0));
             }
         }
     }
+    //interaction button detection
     public void Interact(InputAction.CallbackContext callback)
     {
-        if (callback.started)
-        {
-            interacting = true;
-        }
-        if (callback.canceled)
-        {
-            interacting = false;
-        }
+        if (callback.started){interacting = true;}
+        if (callback.canceled){interacting = false;}
     }
+    //horizontal move dection
     public void LeftMove(InputAction.CallbackContext callback)
     {
-        if (callback.started)
-        {
-            leftJump = true;
-        }
-        if (callback.canceled)
-        {
-            leftJump = false;
-        }
+        if (callback.started){leftJump = true;}
+        if (callback.canceled){leftJump = false;}
     }
     public void RightMove(InputAction.CallbackContext callback)
     {
-        if (callback.started)
-        {
-            rightJump = true;
-        }
-        if (callback.canceled)
-        {
-            rightJump = false;
-        }
+        if (callback.started){rightJump = true;}
+        if (callback.canceled){rightJump = false;}
     }
+    //jumping
     public void Jump(InputAction.CallbackContext callback)
     {
-        if (callback.started)
-        {
-            jumping = true;
-        }
-        if (callback.canceled)
-        {
+        if (callback.started){jumping = true;}
+        if (callback.canceled){
 
             temp = jumpPower;
             jumpPower *= charge;
@@ -131,16 +114,15 @@ public class PlayerBasics : MonoBehaviour
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("wall"))
-        {
+        if (collision.gameObject.CompareTag("wall")){
             Physics2D.gravity = startGravity;
         }
     }
 
-    public IEnumerator WaitEnable(GameObject platform, float t)
+    /*public IEnumerator WaitEnable(GameObject platform, float t)//generic funtion that disables, then renables an object after a given time
     {
         yield return new WaitForSeconds(t);
         platform.gameObject.GetComponent<Collider2D>().enabled = true;
         platform = null;
-    }
+    }*/
 }
