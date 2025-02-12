bd12317e4b8b85bd95d44abd032db67acdf9ba1a
EthanBaalham <146436503+EthanBaalham@users.noreply.github.com>
2025-02-12 10:56:17 +0000

    Quick Collectable fix
    collectables can now only be collected by the player it will check its tag and if tag is "Player" then pick it up.

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -5,9 +5,13 @@ public class Collectables : MonoBehaviour
     public int value;
 
     void OnTriggerEnter2D(Collider2D collision) {
-        if (gameObject.CompareTag("Collectable")) {
-            Destroy(gameObject);
-            CollectableCounter.Instance.IncreaseCollectables(value);
+        if ( collision.CompareTag("Player"))
+        {
+            if (gameObject.CompareTag("Collectable"))
+            {
+                Destroy(gameObject);
+                CollectableCounter.Instance.IncreaseCollectables(value);
+            }
         }
     }
 }
