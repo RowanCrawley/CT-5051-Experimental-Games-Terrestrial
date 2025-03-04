bc57b5e7ec719280ea51762f5adfca51f9ef0743
EthanBaalham <146436503+EthanBaalham@users.noreply.github.com>
2025-03-04 10:36:39 +0000

    Enemy AI Movement
    Enemies now move back and forth on platforms i need to fix a bug the player cant jump if colliding with enemy will look into this.

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,27 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField] GameObject StartPos;
-    [SerializeField] GameObject EndPos;
-    [SerializeField] Rigidbody2D rb2D;
+    //Created by Ethan
+    [Header("Enemy Settings")]  
+    public Rigidbody2D rb;
+    public Vector2 startPos;
+    public Vector2 endPos;
+    public float speed = 1.5f;
+    // will cleary distinguish the editable settings for artists and designers as all vairables are now in a setting.
 
-    public float speed = 2;
+    private Vector2 targetPos;
 
-    private void Start()
-    {
-        Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
-        {
-            //transform.position = new Vector3(26,6,0);
-            //transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
+    private void Awake() {
+        startPos = transform.position;
+        targetPos = endPos;
+        rb = GetComponent<Rigidbody2D>(); // when scene is loaded if the object has a rigidbody it will find the component and equip it. 
     }
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(StartPos.transform.position, 0.5f);
-        Gizmos.DrawWireSphere(EndPos.transform.position, 0.5f);
+    private void FixedUpdate() {
+        Vector2 currentPos = transform.position;
+
+        if(Vector2.Distance(currentPos,endPos) < 0.1f) { // why 0.1f its the closest to 0 so it has a smallest threshhold.
+            targetPos = startPos;
+        }
+        else if (Vector2.Distance(currentPos,startPos) < 0.1f) {                                                  
+            targetPos = endPos;
+            // Vector2 Distance checks if the current pos and target pos
+            // is below the threshhold which is 0.1 essesntially checks if we are close enough to change direction.
+        }
+        Vector2 targetDirection = (targetPos - currentPos).normalized;
+        rb.MovePosition(currentPos + targetDirection * speed * Time.deltaTime); // moves the object towards desired location.
     }
 }
+
