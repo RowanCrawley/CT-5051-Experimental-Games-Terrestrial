c0519487f69ecba9b17e79ca51dc0160c6cfc33c
EthanBaalham <146436503+EthanBaalham@users.noreply.github.com>
2025-03-11 14:44:33 +0000

    Bug Fix Constant enemy damage
    enemy only will deal damage on collision not constant damage the when rolling around the enemy.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //Created by Ethan
 public class PlayerHealth : MonoBehaviour
@@ -8,7 +8,6 @@ public class PlayerHealth : MonoBehaviour
     public Image healthHeart;
     public float currentHealth = 100f;
     public float maxHealth = 100f;
-    private bool isDamaged = false;   
 
     void Start() {
         healthHeart.fillAmount = currentHealth / maxHealth;
@@ -16,28 +15,21 @@ public class PlayerHealth : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Enemies")) {
-            DamageTaken(25f); 
-            isDamaged = true;
+            DamageTaken(25f);
         }// anything with the tag enemies on it will take 25 health away from the player. 
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.CompareTag("HealthPack")) {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("HealthPack")) {
             if (currentHealth < maxHealth) {
                 HealthGained(25f);
                 Destroy(collision.gameObject);
             }
         } // i changed health pack to on trigger instead of collision so when the player is at 100 health they can just walk through the object instead of around it. 
     }
-        private void OnCollisionExit2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Enemies")) {
-            isDamaged = false;  //I have added in a boolean that will determine if the player is in the collider if it is true then damage the player if not then dont.                   
-        }
-    }
     public void DamageTaken(float damage) {
         currentHealth -= damage;
         if (currentHealth < 0) {
-            currentHealth = 0; 
+            currentHealth = 0;
         }
         if (currentHealth == 0) {
             SceneManager.LoadSceneAsync(3);
@@ -46,8 +38,8 @@ public class PlayerHealth : MonoBehaviour
         // damage control function so this will determine how much health to take from the player and when the players health reaches 0 to enter respawn scene. 
     }
     public void HealthGained(float health) {
-            currentHealth += health;
-        
+        currentHealth += health;
+
         if (currentHealth > maxHealth) {
             currentHealth = maxHealth;
         }
