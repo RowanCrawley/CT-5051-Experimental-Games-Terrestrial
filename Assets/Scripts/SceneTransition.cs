f5d49862bcaabeaab57e069c0d6750a6ddb79ab4
EthanBaalham <146436503+EthanBaalham@users.noreply.github.com>
2025-03-24 17:00:14 +0000

    Fixed Scene Transition
    This works by having the scene transition script on the teleporter and typing the number of the scene you want to load this can be found in build settings :)

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -3,9 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    public int Scene;
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            SceneManager.LoadSceneAsync(5);
+            SceneManager.LoadSceneAsync(Scene);
         }
-    }
+    }// detects the player tag and when there is a collision it will teleport to selected scene i have made it public
+     // so just type in the number of the scene on the object and it will work the number from the build settings. 
 }
