1edefae717ace6dc840b001fbb211bb57ddc5a47
EthanBaalham <146436503+EthanBaalham@users.noreply.github.com>
2025-04-29 22:45:13 +0100

    Fixed Coin Collection
    Sound plays on coin collection

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -4,15 +4,16 @@ public class Collectables : MonoBehaviour
 {
     public int value;
     private AudioSource audioSource;
-
-    private void Start() {
-        audioSource = GetComponent<AudioSource>();
-    }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
+
+            audioSource = GetComponent<AudioSource>();
+
             if (gameObject.CompareTag("Collectable")) {
+               
                 audioSource.Play();
-                Destroy(gameObject);
+                Destroy(gameObject, audioSource.clip.length/5); // lets the full audio clip play and i divide so it destrys quicker.
+
                 CollectableCounter.Instance.IncreaseCollectables(value);
                 // adds to the counter i also created a manager counter to help with this. 
             }
